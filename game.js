ca5ab25e9ae4b3282691fab4942066c828d831c5
Robert Bishop <rbishop85@gmail.com>
2022-06-11 22:32:32 -0700

    Used container to simplify win conditions, created container to show full name of choices made, setup recorded win/loss/tie scores, inserted final alert asking player if they wished to play again.

--- game.js
+++ game.js
@@ -1,34 +1,70 @@
-// Choices for computer
-var choices = ["R", "P", "S"];
+// Potential choices
+var compChoices = ["R", "P", "S"];
+
+// Starting value of wins, losses and ties
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+// Object containing each win condition
+var winConditions = {
+    R: "S",
+    S: "P",
+    P: "R"
+};
+// Object containing choice names
+var names = {
+    R: "Rock",
+    S: "Scissors",
+    P: "Paper"
+};
+
+function playGame(){
 
 // Prompt player for choice
-var playerChoice = prompt("What do you choose? R, P or S!");
+var playerChoice = prompt("What do you choose?\n[R]ock, [P]aper or [S]cissors!\n(Please only key in first letter.)");
 // Make sure choice is in Upper Case
 var playerChoice = playerChoice.toUpperCase();
 // Verify that player selected appropriate letter or cancel out
-if ( !choices.includes(playerChoice)){
+if ( !compChoices.includes(playerChoice)){
     alert("That is not a valid choice!")
+    return playGame();
 }
 
 
 alert("Player chose : " + playerChoice)
 // Get a random choice for the computer
-var computer = Math.floor(Math.random() * choices.length);
+var computer = Math.floor(Math.random() * compChoices.length);
 // Broadcast Computer's Choice
-alert("Computer chose " + choices[computer]);
+alert("Computer chose " + compChoices[computer]);
+
+
 // Compare Results
-if (playerChoice === choices[computer]) {
-    alert("It was a tie");
-} else if (playerChoice === "R" && choices[computer] === "S") {
-    alert("Player won!");
-} else if (playerChoice === "P" && choices[computer] === "R") {
-    alert("Player won!");
-} else if (playerChoice === "S" && choices[computer] === "P") {
+if (playerChoice === compChoices[computer]) {
+    ties++;
+    alert("It was a tie!");
+} else if (winConditions[playerChoice] === compChoices[computer]) {
+    wins++;
     alert("Player won!");
 } else {
+    losses++;
     alert("Player lost!");
 }
 
+alert("Stats:\nLosses: "+losses+"\nWins: "+wins+"\nTies: "+ties);
+var isPlayingGame = confirm("Do you want to play again?");
+
+if (isPlayingGame){
+    playGame();
+}
+
+}
+
+playGame();
+
+console.log("Wins: " + wins);
+console.log("Losses: " + losses);
+console.log("Ties: " + ties);
 
 // Compare user's choice to the computer's choice
 
